c7919782e8152354879c6433679ff6d0214a1ad0
ajah <ajahajang00@gmail.com>
2025-10-01 14:07:49 +0200

    Adding to 6kyu

---
+++ 6kyu/findtheparityoutlier.js
@@ -0,0 +1,11 @@
+/*You are given an array (which will have a length of at least 3, but could be very large) containing integers. 
+The array is either entirely comprised of odd integers or entirely comprised of even integers except
+ for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+ */
+const findOutlier = integers => {
+    const even = integers.filter(num => num % 2 === 0)
+    const odd = integers.filter(num => Math.abs(num % 2) === 1)
+    return odd.length === 1 ? odd[0] : even[0]
+
+}
