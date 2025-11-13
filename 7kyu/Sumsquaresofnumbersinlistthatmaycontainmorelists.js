08b7253cbafb5cddee3a754161ff6ae5549b7f5a
ajah <ajahajang00@gmail.com>
2025-11-13 13:29:58 +0200

    Adding to 7kyu

---
+++ 7kyu/Sumsquaresofnumbersinlistthatmaycontainmorelists.js
@@ -0,0 +1,18 @@
+/*Write a function that sums squares of numbers in list that may contain more lists
+Example:
+
+var l = [1,2,3]
+SumSquares(l) == 14
+
+var l = [[1,2],3]
+SumSquares(l) == 14
+
+var l = [[[[[[[[[1]]]]]]]]]
+SumSquares(l) == 1
+
+var l = [10,[[10],10],[10]]
+SumSquares(l) == 400
+Note: your solution must NOT modify the original list */
+function SumSquares(l){
+    return l.flat(Infinity).map(num=>num**2).reduce((a,c)=>a+c,0)
+}
