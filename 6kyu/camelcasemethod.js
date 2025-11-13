2dd11770e95eb8c76cd9d6216ac0434915a28973
ajah <ajahajang00@gmail.com>
2025-11-13 13:16:37 +0200

    Adding to 6kyu

---
+++ 6kyu/camelcasemethod.js
@@ -0,0 +1,8 @@
+/*Write a method (or function, depending on the language) that converts a string to camelCase, that is, all words must have their first letter capitalized and spaces must be removed.
+
+Examples (input --> output):
+"hello case" --> "HelloCase"
+"camel case word" --> "CamelCaseWord" */
+String.prototype.camelCase=function(){
+return this.split(' ').map(word=>word.split('').map((a,i)=>i==0?a=a.toUpperCase():a.toLowerCase()).join('')).join('')
+}
