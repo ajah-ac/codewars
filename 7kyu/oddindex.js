969e181b44be8d829912a20b9e27f09743594df8
ajah <ajahajang00@gmail.com>
2025-11-19 13:53:11 +0200

    Adding to 7kyu

---
+++ 7kyu/oddindex.js
@@ -0,0 +1,9 @@
+/*You are given an array with several "even" words, one "odd" word, and some numbers mixed in.
+
+Determine if any of the numbers in the array is the index of the "odd" word. If so, return true, otherwise false.
+
+ */
+function oddBall(arr){
+  const oddindex=arr.indexOf('odd')
+    return  arr.filter(x=>typeof x==='number').includes(oddindex)
+}
