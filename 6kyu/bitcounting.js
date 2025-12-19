2e3fdd53e4c9946d1a6f0462b031b51c5fad7387
ajah <ajahajang00@gmail.com>
2025-12-19 13:28:20 +0200

    Add to 6 kyu

---
+++ 6kyu/bitcounting.js
@@ -0,0 +1,13 @@
+/**Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case */
+
+
+
+function countBits(n) {
+  return [n.toString()]
+                      .map(num=>(+num)
+                      .toString(2)).join('')
+                      .split('')
+                      .reduce((a,c)=>a+(c==='1'?1:0),0)
+}
