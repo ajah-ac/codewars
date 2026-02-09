c8667effa63460fa5ad5d68ac2d363b00c1fb754
ajah <ajahajang00@gmail.com>
2026-02-09 17:18:39 +0200

    Add to 6 kyu

---
+++ 6kyu/nonEvenSubstrings.js
@@ -0,0 +1,18 @@
+/* Given a string of integers, return the number of odd-numbered substrings that can be formed.
+
+For example, in the case of "1341", they are 1, 1, 3, 13, 41, 341, 1341, a total of 7 numbers.
+
+solve("1341") = 7. See test cases for more examples. */
+function solve(s) {
+  const arr = [];
+
+  for (let i=0;i<s.length;i++) {
+    if (+s[i]%2!== 0) {
+      for (let j=0;j<=i;j++) {
+        arr.push(s.slice(j,i+1));
+      }
+    }
+  }
+
+  return arr.length;
+}
