74560f05e41db4db5d25b8688c0ca2ce844f76b7
ajah <ajahajang00@gmail.com>
2026-03-25 18:06:18 +0200

    Add to 6 kyu

---
+++ 6kyu/EvenOrOddAccessor.js
@@ -0,0 +1,21 @@
+/* Create a function or callable object that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. The function should also return "Even" or "Odd" when accessing a value at an integer index.
+
+For example:
+
+evenOrOdd(2); //'Even'
+evenOrOdd[2]; //'Even'
+evenOrOdd(7); //'Odd'
+evenOrOdd[7]; //'Odd'
+ */
+const evenOrOdd=new Proxy( function (n){
+  return n%2===0?'Even':'Odd'
+},{
+  get(target,prop){
+    const n=Number(prop)
+    if(!isNaN(n)){
+      return n%2===0?'Even':'Odd'
+    }
+    return target[prop]
+ 
+    }
+})
