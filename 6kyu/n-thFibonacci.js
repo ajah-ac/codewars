56afc396dfc4f51df21841b17c26aca5f2c87ff3
ajah <ajahajang00@gmail.com>
2026-04-29 13:46:29 +0200

    Add to 6kyu

---
+++ 6kyu/n-thFibonacci.js
@@ -0,0 +1,18 @@
+/* I love Fibonacci numbers in general, but I must admit I love some more than others.
+
+I would like for you to write me a function that, when given a number n (n >= 1 ), returns the nth number in the Fibonacci Sequence.
+
+For example:
+
+   nthFibo(4) == 2
+Because 2 is the 4th number in the Fibonacci Sequence.
+
+For reference, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of the previous two. */
+const  nthFibo=(n)=> {
+const fib=[0,1]
+for(let i=2;i<=n;i++){
+let num=fib[i-1]+fib[i-2]
+fib.push(num)
+}
+  return fib[n-1]
+}
